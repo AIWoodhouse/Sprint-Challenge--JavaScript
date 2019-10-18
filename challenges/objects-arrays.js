29ec425ac86991ded166115260e359fcb51826d9
AIWoodhouse <a.isablwoodhouse@gmail.com>
2019-10-18 13:03:35 -0700

    Finished objects-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -135,7 +135,16 @@ const zooAnimals = [
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
+
 const displayNames = [];
+
+zooAnimals.forEach(function(animal) {
+
+displayNames.push(`Name: ${animal.animal_name}, Scientific: ${animal.scientific_name}`);
+
+});
+
+
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -156,15 +165,27 @@ The zoos are concerned about animals with a lower population count. Using filter
 
 */
 
+const lowPopulationAnimals = zooAnimals.filter(function(animal) {
+
+  return animal.population < 5;
+  
+  });
+
+console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
 
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
-console.log(populationTotal);
 
+// const populationTotal = 0;
+
+const populationTotal = zooAnimals.reduce(function(totalAnimals, item){
+  return totalAnimals + item.population;
+}, 0);
+
+console.log(populationTotal);
 
 /*
 
