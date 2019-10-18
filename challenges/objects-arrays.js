0a23544f4bdde3031effe3d55b4bee198974de45
AIWoodhouse <a.isablwoodhouse@gmail.com>
2019-10-18 10:06:42 -0700

    Resolved syntax error on classes.js with TL, completed objects on objects-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -12,23 +12,50 @@
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
 
+const tyrannosaurus = {
+  diet: "carnivorous",
+  weight: "7000kg",
+  length: "12m",
+  period: "Late Cretaceous"
+};
+
+const stegosaurus = {
+  diet: "herbivorous",
+  weight: "2000kg",
+  length: "9m",
+  period: "Late Jurassic"
+};
+
+const velociraptor = {
+  diet: "carnivorous",
+  weight: "15kg",
+  length: "1.8m",
+  period: "Late Cretaceous"
+};
+
+
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+
+  tyrannosaurus.roar = function () {
+    return "RAWERSRARARWERSARARARRRR!";
+  }
+
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
@@ -51,8 +78,11 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-console.log(universities);
+
+const universities = graduates.map((university) => {
+  return {'university': university.university};
+});
+
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
